e26bd74509347dde0b9aa062a6ce0d8566fd3332
Barbara Orts <barbaraorts@Barbaras-Air.hsd1.or.comcast.net>
2020-07-16 14:01:51 -0700

    Initial commit

---
+++ home/home.html.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+
+<!--
+File Name: home.html.php
+Date: 7/10/20
+Author: Barbara Orts
+-->
+
+<html lang="en">
+<head>
+    <title>Ace in the Hole Multisport Events</title>
+
+    <meta charset="utf-8">
+    <meta name="author" content="Barbara Orts">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Ace in the Hole Multisport Events website">
+
+    <link rel="stylesheet" href="styles/reset.css" type="text/css">
+    <link rel="stylesheet" href="styles/navigation.css" type="text/css">
+    <link rel="stylesheet/less" href="styles/style.less" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@800&family=Roboto&display=swap"
+          rel="stylesheet">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<header class="header">
+
+    <a href="#home">
+        <img id="logotype" src="images/aith_logo.png" alt="Ace in the Hole Multisport Events logo image"></a>
+
+    <input class="menu-btn" type="checkbox" id="menu-btn"/>
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+
+    <ul class="menu">
+        <li><a href="#home" target="_self">Home</a></li>
+        <li><a href="#about" target="_self">About</a></li>
+        <li><a href="#event" target="_self">Event</a></li>
+        <li><a href="#registration" target="_self">Registration</a></li>
+        <li><a href="#contact" target="_self">Contact</a></li>
+    </ul>
+
+    <div>
+        <img class="hero" src="images/hero_placeholder.jpg" alt="Ace in the Hole Multisport Event Hero image">
+    </div>
+
+</header>
+
+<main>
+
+    <section id="section_one" class="col_one">
+        <h2>Heading 2 (1)</h2>
+        <p>Catasstrophe kitty run to human with blood on mouth from frenzied attack on poor innocent mouse, don't i
+            look
+            cute? suddenly go on wild-eyed crazy rampage for why use post when this sofa is here russian blue and
+            hide
+            from vacuum cleaner so please stop looking at your phone and pet me. Annoy the old grumpy cat, start a
+            fight
+            and then retreat to wash when i lose cats woo. Why must they do that annoy the old grumpy cat, start a
+            fight
+            and then retreat to wash when i lose making bread on the bathrobe. Let me in let me out let me in let me
+            out
+            let me in let me out who broke this door anyway human clearly uses close to one life a night no one naps
+            that long so i revive by standing on chestawaken! or bleghbleghvomit my furball really tie the room
+            together
+            nya nya nyan have a lot of grump in yourself because you can't forget to be grumpy and not be like king
+            grumpy cat. Groom yourself 4 hours - checked, have your beauty sleep 18 hours</p>
+    </section>
+
+    <section class="col_two">
+        <h2>Heading 2 (2)</h2>
+        <p>On human they not getting up ever, licks your face. Spot something, big eyes, big eyes, crouch, shake
+            butt,
+            prepare to pounce. Bite plants damn that dog . Roll on the floor purring your whiskers off when owners
+            are
+            asleep, cry for no apparent reason pretend you want to go out but then don't oooo! dangly balls! jump
+            swat
+            swing flies so sweetly to the floor crash move on wash belly nap walk on car leaving trail of paw prints
+            on
+            hood and windshield. Chill on the couch table lie in the sink all day shred all toilet paper and spread
+            around the house but slap kitten brother with paw for knock over christmas tree. Lick butt bird bird
+            bird
+            bird bird bird human why take bird out i could have eaten that yet cat cat moo moo lick ears lick paws,
+            so
+            sit in box for meow meow ooooh feather moving feather!. Sit on human they not getting up ever. Climb a
+            tree,
+            wait for a fireman jump to fireman then scratch his face. Find something else more interesting cat cat
+            moo
+            moo lick ears lick paws yet run in circles. I see a bird i stare at it i meow at it i do a wiggle come
+            here
+            birdy scream at teh bath attack the dog then pretend like nothing happened. Favor packaging over toy
+            taco
+            cat backwards spells taco cat jump up to edge of bath, fall in then scramble in a mad panic to get out
+            sleep
+            nap. Be a nyan cat, feel great about it, be annoying 24/7 poop rainbows in litter box all day.
+        </p>
+    </section>
+
+    <section class="col_three">
+        <h2>Heading 2 (3)</h2>
+        <p>On human they not getting up ever, licks your face. Spot something, big eyes, big eyes, crouch, shake
+            butt,
+            prepare to pounce. Bite plants damn that dog . Roll on the floor purring your whiskers off when owners
+            are
+            asleep, cry for no apparent reason pretend you want to go out but then don't oooo! dangly balls! jump
+            swat
+            swing flies so sweetly to the floor crash move on wash belly nap walk on car leaving trail of paw prints
+            on
+            hood and windshield. Chill on the couch table lie in the sink all day shred all toilet paper and spread
+            around the house but slap kitten brother with paw for knock over christmas tree. Lick butt bird bird
+            bird
+            bird bird bird human why take bird out i could have eaten that yet cat cat moo moo lick ears lick paws,
+            so
+            sit in box for meow meow ooooh feather moving feather!. Sit on human they not getting up ever. Climb a
+            tree,
+            wait for a fireman jump to fireman then scratch his face. Find something else more interesting cat cat
+            moo
+            moo lick ears lick paws yet run in circles. I see a bird i stare at it i meow at it i do a wiggle come
+            here
+            birdy scream at teh bath attack the dog then pretend like nothing happened. Favor packaging over toy
+            taco
+            cat backwards spells taco cat jump up to edge of bath, fall in then scramble in a mad panic to get out
+            sleep
+            nap. Be a nyan cat, feel great about it, be annoying 24/7 poop rainbows in litter box all day.
+        </p>
+    </section>
+
+    <section class="col_four">
+        <h2>Heading 2 (4)</h2>
+        <h3>Photo Gallery - decoration not JavaScript</h3>
+        <div class="photoGallery">
+            <img class="image1" src="images/ph_300x200.jpg" alt="Bill as a placeholder">
+            <img class="image2" src="images/ph_300x200.jpg" alt="Bill as a placeholder">
+            <img class="image3" src="images/ph_300x200.jpg" alt="Bill as a placeholder">
+            <img class="image4" src="images/ph_300x200.jpg" alt="Bill as a placeholder">
+            <img class="image5" src="images/ph_300x200.jpg" alt="Bill as a placeholder">
+            <img class="image6" src="images/ph_300x200.jpg" alt="Bill as a placeholder">
+        </div>
+    </section>
+
+</main>
+
+<footer>Barbara Orts - Web Design © 2020</footer>
+
+</body>
+</html>
