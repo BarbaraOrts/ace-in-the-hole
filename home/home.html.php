434a61145a224954fb4e20d8b8694a0a86439354
Barbara Orts <barbara.orts@pcc.edu>
2020-07-22 12:55:27 -0700

    Move menu code back to the nav include

--- home/home.html.php
+++ home/home.html.php
@@ -33,18 +33,7 @@ Author: Barbara Orts
 <body>
 
 <header class="header">
-
-    <img id="logotype" class="logo" src="images/aith_logo.png" alt="Ace in the Hole Multisport Events logo image">
-    <nav>
-        <a href="#" class="menu-trigger">Menu</a>
-        <ul>
-            <li><a href="#home" target="_self">Home</a></li>
-            <li><a href="#about" target="_self">About</a></li>
-            <li><a href="#event" target="_self">Event</a></li>
-            <li><a href="#registration" target="_self">Registration</a></li>
-            <li><a href="#contact" target="_self">Contact</a></li>
-        </ul>
-    </nav>
+    <?php include 'includes/nav.inc.html.php'; ?>
 
     <div class="hero">
         <img src="images/hero_placeholder.jpg" alt="Ace in the Hole Multisport Event Hero image">
--- includes/nav.inc.html.php
+++ includes/nav.inc.html.php
@@ -1,4 +1,13 @@
 <?php //define('BASE_URL' , '/cas222/template/'); ?>
 
-
-
+<img id="logotype" class="logo" src="images/aith_logo.png" alt="Ace in the Hole Multisport Events logo image">
+<nav>
+    <a href="#" class="menu-trigger">Menu</a>
+    <ul>
+        <li><a href="#home" target="_self">Home</a></li>
+        <li><a href="#about" target="_self">About</a></li>
+        <li><a href="#event" target="_self">Event</a></li>
+        <li><a href="#registration" target="_self">Registration</a></li>
+        <li><a href="#contact" target="_self">Contact</a></li>
+    </ul>
+</nav>
